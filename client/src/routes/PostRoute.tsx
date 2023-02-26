afaf9057474f36392c24e85bff78f488295576ea
quangminh <quangminh@gmail.com>
2023-02-26 19:44:36 +0700

    br21-filter-categories-post

--- client/src/routes/PostRoute.tsx
+++ client/src/routes/PostRoute.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import PostPage from "../pages/post/PostPage";
 import PostHomePage from "../pages/post";
 import PostId from "../pages/post/PostId";
+import PostCategory from "../pages/post/PostCategory";
 
 type Props = {};
 
@@ -14,6 +15,7 @@ const TasksRoute = (props: Props) => {
           <Route path=":id" element={<PostPage />} />
           <Route path=":id/update" element={<PostId />} />
           <Route path="/create" element={<PostId />} />
+          <Route path="/categories/:id" element={<PostCategory />} />
         </Route>
       </Routes>
     </>
